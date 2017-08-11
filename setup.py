018ac4fd4cf9a98524acb6f44f3c0fa8a1139f12
Grahame Bowland <grahame@angrygoats.net>
2017-08-12 01:50:11 +0800

    initial checkin

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+install_requires = ['SQLAlchemy==1.1.9', 'GeoAlchemy2==0.4.0', 'SQLAlchemy==1.1.9', 'SQLAlchemy-Utils==0.32.14']
+
+setup(
+    author="Grahame Bowland",
+    author_email="grahame@angrygoats.net",
+    description="EAlGIS common functionality (loaders and backend)",
+    long_description="EAlGIS data schema",
+    license="GPL3",
+    keywords="ealgis",
+    url="https://github.com/ealgis/ealgis-data-schema",
+    name="ealgis_common",
+    version="0.0.1",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=install_requires,
+)
