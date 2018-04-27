b1c6a9436620640e9ab78216a3e067493daaeade
Grahame Bowland <grahame@angrygoats.net>
2018-04-27 22:15:04 +0800

    bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     keywords="ealgis",
     url="https://github.com/ealgis/ealgis-data-schema",
     name="ealgis_common",
-    version="0.2.8",
+    version="0.3.0",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=install_requires,
 )
