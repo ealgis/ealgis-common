8ac218bdae08a3b3cc9ac998df50f0098a1001ab
Grahame Bowland <grahame@oreamnos.com.au>
2018-07-03 21:29:25 +0800

    calculate area for GEOMETRY columns

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     keywords="ealgis",
     url="https://github.com/ealgis/ealgis-data-schema",
     name="ealgis_common",
-    version="0.4.4",
+    version="0.4.5",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=install_requires,
 )
