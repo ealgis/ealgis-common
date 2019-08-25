c28bdccbd188e35f7c8b4fc76c4890c3ecb10838
Grahame Bowland <grahame@oreamnos.com.au>
2019-08-25 21:59:14 +0800

    update ealgis-common

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-install_requires = ['SQLAlchemy==1.1.9', 'GeoAlchemy2==0.4.0', 'SQLAlchemy==1.1.9', 'SQLAlchemy-Utils==0.32.14']
+install_requires = ['SQLAlchemy==1.3.7', 'GeoAlchemy2==0.6.3', 'SQLAlchemy-Utils==0.34.2']
 
 setup(
     author="Grahame Bowland",
-    author_email="grahame@angrygoats.net",
+    author_email="grahame@oreamnos.com.au",
     description="EAlGIS common functionality (loaders and backend)",
     long_description="EAlGIS data schema",
     license="GPL3",
     keywords="ealgis",
     url="https://github.com/ealgis/ealgis-data-schema",
     name="ealgis_common",
-    version="0.8.2",
+    version="0.8.3",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=install_requires,
 )
